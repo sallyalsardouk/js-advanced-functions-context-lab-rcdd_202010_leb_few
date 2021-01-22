fa6b13427405f355d8af2a14c0a45b88cd2b207e
sally al sardouk <sally.sardouk98@gmail.com>
2021-01-22 11:08:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,59 @@
  As a result, the lessons for this function will pass *and* it will be available
  for you to use if you need it!
  */
+ let createEmployeeRecord = (arr) => {
+    let employee = {
+        firstName: arr[0],
+        familyName: arr[1],
+        title: arr[2],
+        payPerHour: arr[3],
+        timeInEvents: [],
+        timeOutEvents: [],
+    };
+    return employee;
+}
 
+let createEmployeeRecords = (arr) => {
+    return arr.map(function(employee) {
+        return createEmployeeRecord(employee);
+    })
+}
+
+let createTimeInEvent = function(dateStamp) {
+    let newEvent = {
+        type: "TimeIn",
+        date: dateStamp.split(' ')[0],
+        hour: parseInt(dateStamp.split(' ')[1])
+    };
+    this.timeInEvents.push(newEvent);
+    return this;
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let newEvent = {
+        type: "TimeOut",
+        date: dateStamp.split(' ')[0],
+        hour: parseInt(dateStamp.split(' ')[1])
+    };
+    this.timeOutEvents.push(newEvent);
+    return this;
+}
+
+const hoursWorkedOnDate = function(date) {
+    let timeInEvent = this.timeInEvents.find(e => e.date === date)
+    let hourIn = timeInEvent.hour
+    let timeOutEvent = this.timeOutEvents.find(e => e.date === date)
+    let hourOut = timeOutEvent.hour
+    return parseInt((hourOut - hourIn)/100);
+}
+
+const wagesEarnedOnDate = function(date) {
+    return parseInt(hoursWorkedOnDate.call(this, date) * this.payPerHour);
+}
+
+const findEmployeeByFirstName = (srcArray, firstName) => {
+    return srcArray.find(employee => employee.firstName === firstName);
+}
 let allWagesFor = function () {
     let eligibleDates = this.timeInEvents.map(function (e) {
         return e.date
@@ -19,4 +71,14 @@ let allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+
+const calculatePayroll = (arr) => {
+    let payroll = arr.map(function(employee) {
+        return allWagesFor(employee);
+    });
+    return payroll.reduce(function(accumulator, currentValue) {
+        return accumulator + allWagesFor.call(currentValue)
+    },0);
 }
